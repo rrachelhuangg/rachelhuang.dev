e93069bb2420cfaf818161a3099d80482d2b4f6f
Rachel Huang <rachelhuang@Rachels-Laptop.local>
2026-08-31 15:45:17 -0400

    Updates.

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('homepage.html', active_tab='home')
 
-@app.route('/books')
-def books():
-    return render_template('books.html', active_tab='books')
+# @app.route('/books')
+# def books():
+#     return render_template('books.html', active_tab='books')
 
-@app.route('/portfolio')
-def portfolio():
-    return render_template('portfolio.html', active_tab='portfolio')
+# @app.route('/portfolio')
+# def portfolio():
+#     return render_template('portfolio.html', active_tab='portfolio')
 
 if __name__ == '__main__':
     app.run(debug=True)
